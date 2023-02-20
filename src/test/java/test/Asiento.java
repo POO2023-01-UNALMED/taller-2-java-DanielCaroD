f66b52bf8e31067419df52d5a4250a2c358bb3c4
DanielCaroD <124928694+DanielCaroD@users.noreply.github.com>
2023-02-20 15:52:41 -0500

    Update Asiento.java

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,11 +1,12 @@
 package test;
 
 public class Asiento {
+	
 	String color;
 	int precio;
 	int registro;
 
-	void cambiarColor(color: amarillo) {
-		color = this.color
+	void cambiarColor(Asiento amarillo) {
+		
 	}
 }
