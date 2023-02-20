dd433b3af8e9d71a9a4912eea65cc257d6238c2f
DanielCaroD <124928694+DanielCaroD@users.noreply.github.com>
2023-02-20 17:00:52 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,4 +1,19 @@
 package test;
 
-public class Motor {
+public class Auto {
+	String modelo;
+	int precio;
+	String marca;
+	Motor motor;
+	int registro;
+	int cantidadCreados;
+	
+	int cantidadAsientos() {
+		return cantidadCreados;
+		
+	}
+	
+	String verificarIntegridad() {
+		return marca;
+	}
 }
