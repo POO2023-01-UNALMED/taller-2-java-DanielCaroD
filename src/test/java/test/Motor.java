5bbcce9e34e30c43c68e41c5343b4076cb6b5ad5
DanielCaroD <124928694+DanielCaroD@users.noreply.github.com>
2023-02-20 17:00:03 -0500

    Update Motor.java

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,4 +1,18 @@
 package test;
 
 public class Motor {
+	
+	int numeroCilindos;
+	String tipo;
+	int registro;
+	
+	void CambiarRegistro(int registro) {
+		
+		this.registro = registro;
+	}
+	
+	void asignarTipo(String tipo) {
+		
+		this.tipo = tipo;
+	}
 }
